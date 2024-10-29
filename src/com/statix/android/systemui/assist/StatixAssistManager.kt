c17629a89eff04002a413e6a5d712d70d0e37ac4
Anay Wadhera <anay1018@gmail.com>
2024-10-28 20:47:59 -0400

    StatixSystemUI: Add screenshot action to corner swipe gesture
    Change-Id: Ia52d22caeb25537f2bc0cb31c0f9623706bfb478

--- src/com/statix/android/systemui/assist/StatixAssistManager.kt
+++ src/com/statix/android/systemui/assist/StatixAssistManager.kt
@@ -2,14 +2,19 @@ package com.statix.android.systemui.assist
 
 import android.app.ActivityManager
 import android.content.Context
+import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings.SettingNotFoundException
+import android.view.WindowManager
 import com.android.internal.app.AssistUtils
-import com.android.systemui.assist.AssistManager
+import com.android.internal.util.ScreenshotHelper
 import com.android.systemui.assist.AssistLogger
+import com.android.systemui.assist.AssistManager
 import com.android.systemui.assist.PhoneStateMonitor
 import com.android.systemui.assist.domain.interactor.AssistInteractor
 import com.android.systemui.assist.ui.DefaultUiController
 import com.android.systemui.dagger.SysUISingleton
+import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.model.SysUiState
 import com.android.systemui.recents.OverviewProxyService
@@ -19,8 +24,13 @@ import com.android.systemui.statusbar.CommandQueue
 import com.android.systemui.statusbar.policy.DeviceProvisionedController
 import com.android.systemui.user.domain.interactor.SelectedUserInteractor
 import com.android.systemui.util.settings.SecureSettings
+import com.android.systemui.util.settings.SettingsProxyExt.observerFlow
 import dagger.Lazy
 import javax.inject.Inject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 @SysUISingleton
 class StatixAssistManager
@@ -35,13 +45,14 @@ constructor(
   sysUiState: Lazy<SysUiState>,
   defaultUiController: DefaultUiController,
   assistLogger: AssistLogger,
-  @Main uiHandler: Handler,
+  @Main private val uiHandler: Handler,
   userTracker: UserTracker,
   displayTracker: DisplayTracker,
-  secureSettings: SecureSettings,
+  private val secureSettings: SecureSettings,
   selectedUserInteractor: SelectedUserInteractor,
   activityManager: ActivityManager,
   interactor: AssistInteractor,
+  @Background backgroundScope: CoroutineScope,
 ) :
   AssistManager(
     controller,
@@ -60,4 +71,45 @@ constructor(
     selectedUserInteractor,
     activityManager,
     interactor,
-  ) {}
+  ) {
+
+  private val assistActionFlow =
+    secureSettings
+      .observerFlow(ASSIST_ACTION)
+      .map { getAssistInt() }
+      .stateIn(backgroundScope, started = SharingStarted.Eagerly, initialValue = getAssistInt())
+
+  private val screenshotHelper = ScreenshotHelper(context)
+
+  private fun getAssistInt(): Int {
+    return try {
+      secureSettings.getInt(ASSIST_ACTION)
+    } catch (e: SettingNotFoundException) {
+      -1
+    }
+  }
+
+  override fun startAssist(args: Bundle) {
+    val invocationType = args.getInt(AssistManager.INVOCATION_TYPE_KEY, 0)
+    if (invocationType == AssistUtils.INVOCATION_TYPE_GESTURE) {
+      // Check what action we should be doing.
+      when (assistActionFlow.value) {
+        0 -> {
+          // Take screenshot
+          screenshotHelper.takeScreenshot(
+            WindowManager.ScreenshotSource.SCREENSHOT_VENDOR_GESTURE,
+            uiHandler,
+            null,
+          )
+          return
+        }
+        else -> return super.startAssist(args)
+      }
+    }
+    return super.startAssist(args)
+  }
+
+  companion object {
+    private val ASSIST_ACTION = "assist_action"
+  }
+}
